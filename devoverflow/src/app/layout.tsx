b781af9775d0207d53bd1c6fdda851bc62c0c470
Tien Vo <tienhoangvo.dev@gmail.com>
2024-11-08 01:22:03 +0700

    feat: add assets, metadata, and theming to project

--- devoverflow/src/app/layout.tsx
+++ devoverflow/src/app/layout.tsx
@@ -2,21 +2,24 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const inter = localFont({
+  src: "./fonts/InterVF.ttf",
+  variable: "--font-inter",
+  weight: "300 400 500 600 700",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const spaceGrotesk = localFont({
+  src: "./fonts/SpaceGroteskVF.ttf",
+  variable: "--font-inter",
+  weight: "300 400 500 600 700",
 });
 
 export const metadata: Metadata = {
   title: "DevOverflow",
   description:
     "DevOverflow is a modern StackOverflow-inspired web application built using Next.js 15, TypeScript, TailwindCSS, MongoDB, and more. This project showcases advanced features like AI-generated answers, a post recommendation system, badge and reputation mechanics, and comprehensive filtering and pagination. Developed as part of a JSMastery tutorial led by Adrian Hajdin.",
+  icons: {
+    icon: "/images/site-logo.svg",
+  },
 };
 
 const RootLayout = ({
@@ -27,7 +30,7 @@ const RootLayout = ({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} ${spaceGrotesk.variable} antialiased`}
       >
         {children}
       </body>
